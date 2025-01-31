b4d64fe7e946e12248ed2b7565a4d40f8c3c0c04
robertsmith2018 <oakcodes@gmail.com>
2025-01-31 02:07:53 +0200

    Add all project files and subfolders

---
+++ my-blog-backend/src/build/precache-manifest.230df09f88299eb52af435f936f9f014.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "38f204bd6da0da10c5a0",
+    "url": "/static/css/main.10c73eda.chunk.css"
+  },
+  {
+    "revision": "38f204bd6da0da10c5a0",
+    "url": "/static/js/main.e8b38a27.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d6551457c70e6463294c",
+    "url": "/static/js/2.09498be9.chunk.js"
+  },
+  {
+    "revision": "68dab9b131c49728eef96127096269cc",
+    "url": "/index.html"
+  }
+];
